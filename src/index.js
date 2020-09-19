a24035db0e8089dfbafff65cfb595317ca5de98a
ISTANBULBEKLE <ekipkalir@gmail.com>
2020-09-19 10:25:31 +0100

    Update index.js
    src/index.js is aded

--- src/index.js
+++ src/index.js
@@ -1,13 +1,16 @@
+import 'stop-runaway-react-effects/hijack'
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+ReactDOM.render( <
+  React.StrictMode >
+  <
+  App / >
+  <
+  /React.StrictMode>,
   document.getElementById('root')
 );
 
